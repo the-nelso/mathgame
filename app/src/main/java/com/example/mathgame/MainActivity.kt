d05a9b7b38dc4b41657c7f82b85306292d1eaabb
Ana Karolina do Vale <anavale@rpc.com.br>
2024-10-11 20:17:17 -0300

    feat: gerar numero aleatório

--- app/src/main/java/com/example/mathgame/MainActivity.kt
+++ app/src/main/java/com/example/mathgame/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mathgame
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,20 +18,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val textViewNum1: TextView = findViewById(R.id.textViewNum1)
-        val textViewNum2: TextView = findViewById(R.id.textViewNum2)
-        val textViewResultado: TextView = findViewById(R.id.textViewResultado)
-        val buttonSoma: Button = findViewById(R.id.buttonSoma)
+        val textViewNum1  = findViewById<TextView>(R.id.textView)
+        val textViewNum2  = findViewById<TextView>(R.id.textView3)
+        val clientResult = findViewById<EditText>(R.id.editTextNumber).toString().toInt()
+        val textViewResultado = findViewById<TextView>(R.id.textView5)
+        val buttonSoma = findViewById<Button>(R.id.button)
+        layoutPrincipal = findViewById<View>(R.id.main)
 
         buttonSoma.setOnClickListener {
             val num1 = textViewNum1.text.toString().toInt()
             val num2 = textViewNum2.text.toString().toInt()
             val resultado = somar(num1, num2)
-            textViewResultado.text = "Resultado: $resultado"
+            textViewResultado.text = "$resultado"
+            if(clientResult == resultado) {
+                layoutPrincipal.setBackgroundColor(Color.GREEN)
+            } else {
+                layoutPrincipal.setBackgroundColor(Color.RED)
+            }
         }
     }
 
     private fun somar(num1: Int, num2: Int): Int {
         return num1 + num2
+    }
+
+    private fun gerarNumeroAleatorio(min: Int, max: Int): Int {
+        return Random.nextInt(min, max + 1)
     }
 }
